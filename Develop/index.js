7b2dd7f9afc24ba2cd0b019a9ceba4272b5f6d21
Christian Sanchez <csanchezmartinez486@gmail.com>
2022-01-17 21:12:02 -0500

    wrote writeToFile function. error message will be given if .md file cannot be generated.

--- Develop/index.js
+++ Develop/index.js
@@ -85,11 +85,6 @@ init();
 // WHEN I choose a license for my application from a list of options
 // THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
 
-// Questions to ask user:
-// WHEN I enter my GitHub username
-// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
-// WHEN I enter my email address
-// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
 
 // Create links that redirect the table of contents to the correct section.
 // WHEN I click on the links in the Table of Contents
